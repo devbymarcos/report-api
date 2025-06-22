cf76cfd7ea55b9c241870e63c55b5861aeb1e276
Marcos <marcoslopes.dev@gmail.com>
2025-06-22 17:51:27 -0300

    feat: update entry point in tsup configuration from src/index.ts to src/server.ts

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts"],
+  entry: ["src/server.ts"],
   format: ["cjs"], // <- aqui o importante
   target: "node22",
   outDir: "dist",
