b3991e814c884db7117fc2d81335ee159ae3e4e1
devbymarcos <marcoslopes.dev@gmail.com>
2025-06-30 12:20:26 -0300

    fix: add missing newline for better code readability in index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { fileURLToPath } from "url";
 import path from "path";
 import dotenv from "dotenv";
 import cors from "@fastify/cors";
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
