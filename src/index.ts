b5e439f3552564aa53a920c0cd30d4b5596b42d0
devbymarcos <marcoslopes.dev@gmail.com>
2025-06-30 12:11:38 -0300

    fix: remove unnecessary blank line in index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ import { fileURLToPath } from "url";
 import path from "path";
 import dotenv from "dotenv";
 import cors from "@fastify/cors";
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
